be092227f9f087a0f4792f452408f622072f3767
Whisky Tango Foxtrot <anhdungadg@gmail.com>
2024-08-07 01:40:58 +0700

    I don't know it always show the error

--- research/src/simple-tfidf/main.py
+++ research/src/simple-tfidf/main.py
@@ -34,3 +34,14 @@ df_idf = pd.DataFrame(tfidf_transformer.idf_, index=count.get_feature_names_out(
 #inverse document frequency
 df_idf.sort_values(by=['idf_weights'])
 print("df_idf", df_idf)
+
+
+#tfidf
+tf_idf_vector=tfidf_transformer.transform(word_count)
+feature_names = count.get_feature_names()
+
+first_document_vector=tf_idf_vector[1]
+df_tfifd= pd.DataFrame(first_document_vector.T.todense(), index=feature_names, columns=["tfidf"])
+
+df_tfifd.sort_values(by=["tfidf"],ascending=False)
+print("df_tfifd", df_tfifd)
