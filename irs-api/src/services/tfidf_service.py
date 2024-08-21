45d0af0851c12f501a8a87821257e26a81594d47
Anh Dung <hi@anhdung.me>
2024-08-22 03:17:39 +0700

    Update TfidfService

--- irs-api/src/services/tfidf_service.py
+++ irs-api/src/services/tfidf_service.py
@@ -13,8 +13,26 @@
 
 
 # src/services/tfidf_service.py
+
+# import math
+# import numpy as np
+# # Module distance từ thư viện scipy để tính toán các loại khoản cách giữa các điểm
+# from scipy.spatial import distance
+# # Module TfidfVectorizer để biến đổi text thành ma trận các đặc trưng TF-IDF
+# from sklearn.feature_extraction.text import TfidfVectorizer
+
+
 # Non repository-based implementation
 class TfidfService:
     def calculate_tfidf(self, text):
+        docs = [
+            'data mining is awesome. data mining helps to find frequent itemsets in database.',
+            'information retrieval is cool. information retrieval helps to search data quickly.',
+            'natural language processing is interesting. it helps computer to better understand text.'
+        ];
+
+        query = text['query']
+        
+
         # Implement your TF-IDF calculation logic here
-        return {"data": text, "tfidf_score": 0.5, "page_rank": 0.8}
+        return {"data": text['query'], "tfidf_score": 0.5, "page_rank": 0.8}
